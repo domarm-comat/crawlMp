d10dc7f830c3d17b7ae20272708a190c700bbd03
Martin Domaracky <puffisional@gmail.com>
2021-11-28 18:22:27 +0100

    - code refactoring

--- crawlMp/__init__.py
+++ crawlMp/__init__.py
@@ -1,6 +1,8 @@
+from multiprocessing import Manager
 from time import time_ns
 
 start_time = time_ns()
+share_manager = Manager()
 
 
 class CrawlException(Exception):
--- crawlMp/sources/crawlerManager.py
+++ crawlMp/sources/crawlerManager.py
@@ -1,102 +1,10 @@
-from multiprocessing import Manager, Event, Process, Lock
+from multiprocessing import Event, Lock
 from threading import Thread
 from typing import Any, Optional, Callable
 
 # Create global Process Manager
-from crawlMp.sources.results import Results
-
-main_manager = Manager()
-
-
-def worker_id_gen():
-    worker_id = 0
-    while True:
-        yield worker_id
-        worker_id += 1
-
-
-class CrawlWorker(Process):
-    id_gen = worker_id_gen()
-    jobs_acquiring_lock = Lock()
-    targets_found = main_manager.list()
-    links_followed = main_manager.list()
-    links_failed = main_manager.list()
-
-    def __init__(self, crawler_class, sig_worker_idle: Event, jobs_list: list, buffer_size: int = 64, *args: Any,
-                 **kwargs: Any) -> None:
-        Process.__init__(self)
-        self.worker_id = next(self.id_gen)
-        self.stop_signal = Event()
-        self.wake_signal = Event()
-        self.buffer_size = buffer_size
-        self.crawler_class = crawler_class
-        self.sig_worker_idle = sig_worker_idle
-        self.jobs_list = jobs_list
-        self.args = args
-        self.kwargs = kwargs
-
-    def run(self) -> None:
-        """
-        Worker body
-        Create Crawler object and crawl through.
-        :return: None
-        """
-        # Initiate Crawler object for current Worker
-        crawler = self.crawler_class(*self.args, **self.kwargs)
-
-        def flush_results():
-            self.targets_found += crawler.targets_found
-            self.links_followed += crawler.links_followed
-            self.links_failed += crawler.links_failed
-            crawler.targets_found = []
-            crawler.links_followed = []
-            crawler.links_failed = []
-
-        iterations = 0
-        # Crawl until wake_signal is high
-        while self.wake_signal.wait():
-            if self.stop_signal.is_set():
-                # Check if Worker should stop
-                break
-            elif crawler.links:
-                # Crawl next link
-                next(crawler)
-                if self.sig_worker_idle.is_set() and len(crawler.links) > self.buffer_size:
-                    # One of the workers is IDLE and Worker has more links than buffer size
-                    with self.jobs_acquiring_lock:
-                        # Keep links of buffer size
-                        self.jobs_list += crawler.links[self.buffer_size:]
-                        # Remove those links from crawler links
-                        del crawler.links[self.buffer_size:]
-                iterations += 1
-                if iterations % self.buffer_size == 0:
-                    flush_results()
-            elif not self.jobs_list:
-                # job_queue is empty
-                # set wake_signal to low
-                self.wake_signal.clear()
-                # set worker_idle_signal to high
-                self.sig_worker_idle.set()
-            else:
-                # Crawler has no links to follow, but there are some links already in job_queue
-                with self.jobs_acquiring_lock:
-                    # Fill crawler.links from jobs_list of buffer_size
-                    crawler.links += self.jobs_list[:self.buffer_size]
-                    # Remove fetched links from jobs_list
-                    del self.jobs_list[:self.buffer_size]
-        flush_results()
-
-    def stop(self) -> None:
-        """
-        Stop worker
-        :return:
-        """
-        self.stop_signal.set()
-
-    def get_results(self):
-        return Results(targets_found=self.targets_found,
-                       links_followed=self.links_followed,
-                       links_failed=self.links_failed)
+from crawlMp import share_manager
+from crawlMp.sources.crawlWorker import CrawlWorker
 
 
 class CrawlerManager:
@@ -105,7 +13,7 @@ class CrawlerManager:
         assert num_proc > 0
         self.num_proc = num_proc
         self.crawler_class = crawler_class
-        self.jobs_list = main_manager.list(links)
+        self.jobs_list = share_manager.list(links)
         self.sig_worker_idle = Event()
         self.jobs_acquiring_lock = Lock()
         self.args = args
@@ -165,11 +73,12 @@ class CrawlerManager:
             # Wait until all workers are finished
             worker.join()
 
+        output = worker.get_results()
         # Call the Callback if necessary
         if callback is not None:
-            callback(worker.get_results())
+            callback(output)
         else:
-            return worker.get_results()
+            return output
 
     def start(self, callback: Callable = None) -> Optional[CrawlWorker]:
         """
@@ -179,7 +88,6 @@ class CrawlerManager:
         :return: CrawlWorker or None
         """
         if callback is None:
-            print("now")
             return self._start()
         else:
             assert callable(callback)
