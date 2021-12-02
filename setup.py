4896fffd901af6ee10db9648ed316da499d4957b
Martin Domaracky <puffisional@gmail.com>
2021-12-02 14:15:02 +0100

    - update setup.py

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@ setuptools.setup(
     description="Multi Process Crawl toolset",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://gitlab.comat.sk/domarm/crawlMp",
+    url="https://github.com/domarm-comat/crawlMp",
     packages=setuptools.find_packages(),
+    package_data={'crawlMp.tests': ['fs_files.txt']},
     scripts=['crawlMp/scripts/search_fs_mp'],
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -24,6 +25,9 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
     ],
     install_requires=[
+        "coverage>=6.2",
+        "pyfakefs>=4.5.3",
+        "pytest>=6.2.5",
     ],
     extras_requires={
     },
